1b015302050c3e98b623f1dfac2f170f5df73577
lubomir-sokolovsky-sde <lubomir.sokolovsky@sde.cz>
2017-01-02 13:22:24 +0100

    Add secure password to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,9 @@ class User < ActiveRecord::Base
   validates :username, presence: true,
             uniqueness: { case_sensitive: false},
             length: {minimum: 3, maximum: 25}
-  vakudates :email, presence: true,
+  validates :email, presence: true,
             length: { maximum: 105 },
             uniqueness: { case_sensitive: false},
             format: { with: VALID_EMAIL_REGEX }
+  has_secure_password
 end
