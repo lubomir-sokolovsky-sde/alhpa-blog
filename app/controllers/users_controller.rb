e5d9c9593cbd38f39125e0ffabd1d1745c106e0f
lubomir-sokolovsky-sde <lubomir.sokolovsky@sde.cz>
2017-01-03 16:25:44 +0100

    Add users listing and update article show view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,4 @@
 class UsersController < ApplicationController
-  def new
-    @user = User.new
-  end
-
   def create
     @user = User.new(user_params)
     if @user.save
@@ -15,6 +11,14 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
+  end
+
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
   end
 
   def show
